664ce9ed270b562f76dabcb681df02f700664209
take-PC\take <myth8763@yahoo.co.jp>
2017-08-10 14:00:00 +0900

    a

---
+++ Besuda 3D/Assets/Script/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 A = new;
+        Vector3(target.position.x, target.position.y, target.position.z);
+        transform.position = target.position;
+        
+	}
+}
